092b1b4e1326a9325ea7aed8ddda00d2bd632fb4
alexander <alexander.otsala@gmail.com>
2024-12-06 16:32:03 +0200

    Add source files

---
+++ helper.c
@@ -0,0 +1,44 @@
+#include "fractol.h"
+
+int	exits(t_info *info)
+{
+	(void)info;
+	exit(0);
+}
+
+void	my_mlx_pixel_put(t_info *info, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = info->addr
+		+ (y * info->line_length + x * (info->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
+
+void	init_fractal(t_info *info, void *mlx, void *win)
+{
+	info->mlx = mlx;
+	info->win = win;
+	info->width = WIN_WIDTH;
+	info->height = WIN_HEIGHT;
+	info->all_pixels = WIN_WIDTH * WIN_HEIGHT;
+	info->iterations = ITERATIONS;
+	info->zoom = 0.3;
+	info->wildcard = 252;
+}
+
+void	reset(t_info *info)
+{
+	if (info->set == 'm' || info->set == 'b')
+	{
+		info->zoom = 0.3;
+		info->x_pos = -0.5;
+		info->y_pos = 0;
+	}
+	else
+	{
+		info->zoom = 0.3;
+		info->x_pos = 0;
+		info->y_pos = 0;
+	}
+}
